edac6a83b7fc986856e4fc33d6bd62776701f201
j1014park <hero20061014@gmail.com>
2023-07-07 11:29:12 +0900

    Read 구현 및 Mock 구현 Commit

--- DeviceDriver/FlashMemoryDevice.h
+++ DeviceDriver/FlashMemoryDevice.h
@@ -3,6 +3,8 @@
  * This class represents the interface to a Flash Memory Device. The hardware has only two methods - 'read' and 'write'
  * However, the interface for using the device is a lot more complex than that. It is outlined in the top-level README file.
  */
+
+#include <stdexcept>
 #define interface struct
 
 interface FlashMemoryDevice
