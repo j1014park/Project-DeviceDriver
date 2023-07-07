706b2d0a0b9b2ad5cd747c69b21dd97af36a4b48
j1014park <hero20061014@gmail.com>
2023-07-07 10:12:43 +0900

    first Commit

---
+++ DeviceDriver/DeviceDriver.cpp
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "FlashMemoryDevice.h"
+
+class DeviceDriver
+{
+public:
+    DeviceDriver(FlashMemoryDevice* hardware);
+    int read(long address);
+    void write(long address, int data);
+
+protected:
+    FlashMemoryDevice* m_hardware;
+};
---
+++ DeviceDriverMockTest/DeviceDriverMockTest.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "../DeviceDriver/DeviceDriver.cpp"
+#include <vector>
+
+using namespace testing;
+using namespace std;
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
